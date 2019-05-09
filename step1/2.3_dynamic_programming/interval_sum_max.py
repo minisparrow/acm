ce57e65910b18507ab4cd487121a98d3725616d2
minisparrow <melchior_faith@163.com>
2019-05-10 07:10:15 +0800

    details

--- step1/2.3_dynamic_programming/interval_sum_max.py
+++ step1/2.3_dynamic_programming/interval_sum_max.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 
 arr = [1,2,4,1,7,8,3]
+arr = [4,1,1,9,1,2,3,4,6,2,9]
 
 #---------------------------------------------------------
 # recursive opt
@@ -22,33 +23,28 @@ t1 = time.time()
 for i in range(10000):
    (rec_opt(arr,len(arr)-1))
 t1 = time.time() - t1
-print("time:", t1)
+print("recursive time:", t1)
+print(rec_opt(arr,len(arr)-1))
 
 
 #---------------------------------------------------------
 # dynamic programming 
 #---------------------------------------------------------
-def dp_opt(arr,i):
+def dp_opt(arr):
     dp = np.zeros(len(arr))
-    if i == 0:
-        return arr[0]
-    elif i == 1:
-        return max(arr[0],arr[1])
-    else:
-        A = dp_opt(arr,i-2) + arr[i]
-        B = dp_opt(arr,i-1) 
+    dp[0] = arr[0]
+    dp[1] = max(arr[0],arr[1])
+    for i in range(2,len(arr)):
+        A = dp[i-2] + arr[i]
+        B = dp[i-1] 
         dp[i] = max(A,B)
-        return dp[i]
-
-
+    return dp[len(arr)-1]
 
 t2 = time.time()
 for i in range(1000):
-   (dp_opt(arr,len(arr)-1))
+   (dp_opt(arr))
 t2 = time.time() - t2
-print("time:", t2)
+print("dynamic time:", t2)
+print(dp_opt(arr))
 
-#---------------------------------------------------------
-# time compare
-#---------------------------------------------------------
-print("dp faster that recursive %f times" % (t1/t2 ))
+print("times",t1/t2)
